040ce09d43816118a67dfbee4a6b58bdea3c4ac2
Alfonso <alfonsoramos.1803@gmail.com>
2022-06-26 17:17:48 -0600

    Maquetacion del Home y estilos

--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -1,20 +1,30 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import {Home, Checkout, Information, Payment, Success, NotFound} from '../containers/'
+import {
+  Home,
+  Checkout,
+  Information,
+  Payment,
+  Success,
+  NotFound,
+} from '../containers/';
+import Layout from '../components/Layout';
 
 const App = () => {
   return (
     <BrowserRouter>
+      <Layout>
         <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/checkout" element={<Checkout />} />
-            <Route path="/checkout/information" element={<Information />} />
-            <Route  path="/checkout/payment" element={<Payment />} />
-            <Route path="/checkout/success" element={<Success />} />
-            <Route path='*' element={<NotFound/>}/>
+          <Route path="/" element={<Home />} />
+          <Route path="/checkout" element={<Checkout />} />
+          <Route path="/checkout/information" element={<Information />} />
+          <Route path="/checkout/payment" element={<Payment />} />
+          <Route path="/checkout/success" element={<Success />} />
+          <Route path="*" element={<NotFound />} />
         </Routes>
+      </Layout>
     </BrowserRouter>
-  )
-}
+  );
+};
 
-export default App
+export default App;
